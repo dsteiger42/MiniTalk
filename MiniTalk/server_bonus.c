5a25942c3544a7a02bf1393722c069e66ef29e02
dsteiger42 <167896122+dsteiger42@users.noreply.github.com>
2024-11-04 20:01:37 +0000

    Update server_bonus.c

--- MiniTalk/server_bonus.c
+++ MiniTalk/server_bonus.c
@@ -1,49 +1,96 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <signal.h>
-#include <string.h>
+#include "minitalk.h"
 
-void	ft_btoa(int sig, siginfo_t *info, void *context)
+static void	print_message(unsigned char *buffer, size_t size)
 {
-	static int	bit;
-	static int	i;
+	buffer[size] = '\0';
+	ft_printf("Received message: %s\n", buffer);
+	free(buffer);
+	buffer = NULL; // reset buffer pointer
+}
 
+void	print_error_and_exit2(char *message)
+{
+	ft_putstr_fd(message, 2);
+	exit(1);
+}
+
+
+void	ft_btoa(int sig, siginfo_t *siginfo, void *context)
+{
+	static t_buffer	buf = {NULL, 0, 8, 0, 0}; // Start with an initial capacity of 8
+	size_t			new_capacity;
+	unsigned char	*new_buffer;
+	size_t			i;
+
+	(void)siginfo;
 	(void)context;
-	if (sig == SIGUSR1)
-		i |= (0x01 << bit);
-	bit++;
-	if (bit == 8)
+
+	// Allocate initial buffer if it doesn't exist
+	if (buf.buffer == NULL)
 	{
-		if (i == 0)
-			kill(info->si_pid, SIGUSR2);
-		ft_printf("%c", i);
-		bit = 0;
-		i = 0;
+		buf.buffer = (unsigned char *)malloc(buf.capacity);
+		if (!buf.buffer)
+			return ;
+	}
+
+	if (sig == SIGUSR1)
+		buf.byte |= (0x01 << buf.bit);
+	buf.bit++;
+	
+	if (buf.bit == 8) // When we've received a full byte
+	{
+		if (buf.byte == '\0') // End of message
+		{
+			print_message(buf.buffer, buf.size);
+			free(buf.buffer); // Free buffer after printing
+			buf.buffer = NULL; // Reset buffer to allow new allocation on next message
+			buf.size = 0;
+			buf.capacity = 8; // Reset capacity for the next message
+		}
+		else
+		{
+			// Resize buffer if necessary
+			if (buf.size == buf.capacity)
+			{
+				new_capacity = buf.capacity * 2;
+				new_buffer = (unsigned char *)malloc(new_capacity);
+				if (!new_buffer)
+				{
+					free(buf.buffer);
+					buf.buffer = NULL;
+					return ;
+				}
+				for (i = 0; i < buf.size; i++) // Copy old data
+					new_buffer[i] = buf.buffer[i];
+				free(buf.buffer);
+				buf.buffer = new_buffer;
+				buf.capacity = new_capacity;
+			}
+			// Add byte to buffer
+			buf.buffer[buf.size++] = buf.byte;
+		}
+		// Reset bit and byte for the next character
+		buf.bit = 0;
+		buf.byte = 0;
 	}
 }
 
-int	main(int argc, char **argv)
+int	main(void)
 {
 	int					pid;
-	struct sigaction	act;
+	struct sigaction	sa;
 
-	(void)argv;
-	if (argc != 1)
-	{
-		ft_printf("Error\n");
-		return (1);
-	}
 	pid = getpid();
-	ft_printf("%d\n", pid);
-	act.sa_sigaction = ft_btoa;
-	sigemptyset(&act.sa_mask);
-	act.sa_flags = 0;
-	while (argc == 1)
-	{
-		sigaction(SIGUSR1, &act, NULL);
-		sigaction(SIGUSR2, &act, NULL);
+	ft_printf("PID: %d\n", pid);
+	sa.sa_sigaction = ft_btoa;
+	sa.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+		print_error_and_exit2("Error setting SIGUSR1 handler");
+	if (sigaction(SIGUSR2, &sa, NULL) == -1)
+		print_error_and_exit2("Error setting SIGUSR2 handler");
+	while (1)
 		pause();
-	}
 	return (0);
 }
