74e96682b1ef16b4ebce33fa9b07a160a1ed5362
dsteiger42 <167896122+dsteiger42@users.noreply.github.com>
2024-11-04 19:56:51 +0000

    Update minitalk.h

--- MiniTalk/minitalk.h
+++ MiniTalk/minitalk.h
@@ -1,17 +1,26 @@
 #ifndef MINITALK_H
 # define MINITALK_H
-# define BUFFER_SIZE 1024
 
 # include "ft_printf/ft_printf.h"
 # include "libft/libft.h"
 # include <signal.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <sys/types.h>
 # include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+#include <stdlib.h>
 
-void	ft_atob(unsigned char c, int pid);
+typedef struct s_buffer
+{
+    unsigned char	*buffer;	// Pointer to the data buffer
+    size_t size;				// Current size of the buffer (number of bytes used)
+	size_t capacity;			// total capacity of the buffer
+    int bit;					// Current bit position
+    int byte;					// Current byte value
+}					t_buffer;
+
 void	print_error_and_exit(char *message);
-void	ft_btoa(int sig);
+void	ft_atob(unsigned char c, int pid);
+void	print_error_and_exit2(char *message);
+void	ft_btoa(int sig, siginfo_t *siginfo, void *context);
 
 #endif
